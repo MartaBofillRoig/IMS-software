92ab19e42a1c68a12d65b97a498c4ac452036e83
Pavla Krotka <pavla.krotka@gmail.com>
2022-06-22 17:42:11 +0200

    ToyPackage

---
+++ ToyPackage/R/say_hi.R
@@ -0,0 +1,18 @@
+#' Function that says hi!
+#'
+#' @description This function prints "Hello world"
+#'
+#' @export
+#' 
+#' @examples 
+#' 
+#' say_hi()
+#'
+#'
+#' @return String "Hello world"
+#' @author Pavla Krotka
+
+
+say_hi <- function(){
+  print("Hello world!")
+}
